5ce39149d39176b1db18b9975c96141ccc0b6f6c
HugoChSa <hchansaba@ieschandomonte.edu.es>
2026-02-12 13:18:17 +0100

    rol

---
+++ src/ud4/e0709/Bombilla.java
@@ -0,0 +1,48 @@
+package ud4.e0709;
+
+public class Bombilla {
+
+    private boolean interruptor;
+
+    private static boolean interruptorGeneral;
+
+    boolean estaEncendida(){
+        return interruptor;
+    }
+
+    void encender() {
+        interruptor = true;
+    }
+
+    void apagar() {
+        interruptor = false;
+    }
+
+
+    static void encenderInterruptorGeneral() {
+        interruptorGeneral = true;
+
+    }
+
+    static void apagarInterruptorGeneral(){
+        interruptorGeneral = false;
+    }
+
+    static boolean getInterruptorGeneral(){
+        return interruptorGeneral;
+    }
+
+
+    public static void main(String[] args) {
+        Bombilla b1 = new Bombilla();
+        
+        b1.encender();
+
+
+        System.out.println("La bombilla está encendida? " + b1.estaEncendida());
+        //System.out.println("Interruptor general: " + Bombilla.interruptorGeneral());
+    
+    }
+    
+
+}
--- src/ud4/rol/CombateSingular.java
+++
@@ -1,5 +0,0 @@
-package ud4.rol;
-
-public class CombateSingular {
-    
-}
