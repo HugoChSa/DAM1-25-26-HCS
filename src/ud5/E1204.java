a3066b5aa3eb2582162c28c5a81c6df89708695c
HugoChSa <hchansaba@ieschandomonte.edu.es>
2026-03-27 12:23:15 +0100

    Ud5

---
+++ src/ud5/E1204.java
@@ -0,0 +1,29 @@
+package ud5;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class E1204<Numero> {
+    int numero;
+    
+    
+    public E1204(int numero) {
+        this.numero = numero;
+    }
+
+    List<Numero> listaNums = new ArrayList<>();
+
+    Collection<Numero> coleccionNums = listaNums;
+
+    boolean add(Numero numero) {
+        if ( == -1) {
+            
+        }
+        return false;
+        
+    }
+}
+
+
